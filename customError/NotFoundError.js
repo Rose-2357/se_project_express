c0628085c7ca0507971040b7366f9a7f20c3fe8f
RoseGhaffari <aradsystem995@gmail.com>
2026-02-07 15:09:59 -0600

    fix: use clasical inheritance for NotFoundError

--- customError/NotFoundError.js
+++ customError/NotFoundError.js
@@ -1,12 +1,8 @@
-function NotFoundError(message) {
-  this.message = message;
-  this.name = "NotFoundError";
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "NotFoundError";
+  }
 }
-
-/* eslint-disable no-proto */
-// no-proto is disabled to allow prototypal inheritance
-// prototypal inharitance is used because node.js uses commonJS
-NotFoundError.__proto__ = Error.prototype;
-/* eslint-enable */
 
 module.exports = NotFoundError;
