04b97c488dce6c1a8652e171814523dccace3995
RoseGhaffari <aradsystem995@gmail.com>
2026-04-01 02:21:46 -0500

    fix: add default error message

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 module.exports = function errorHandler(err, req, res, next) {
   console.log(err);
-  const { statusCode = 500, message } = err;
+  const { statusCode = 500, message = "Internal Server Error" } = err;
   return res.status(statusCode).send({ message });
 };
